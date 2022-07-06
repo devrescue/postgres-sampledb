4490e0eefafd761ff1c497cd0b6aa5fa0d48cc33
devrescue <ko.devrescue@gmail.com>
2022-07-06 16:33:12 -0400

    fixed field lengths

--- create_tables.sql
+++ create_tables.sql
@@ -2,33 +2,30 @@ CREATE SCHEMA HealthStats
     CREATE TABLE HealthyLifeExpectancyAtBirth(
         "Location" VARCHAR(100),
         "Period" INTEGER,
-        "Indicator" VARCHAR(50),
+        "Indicator" VARCHAR(100),
         "Dim1" VARCHAR(100),
         "First Tooltip" DOUBLE PRECISION
     )
-
+ 
     CREATE TABLE TobaccoAge(
         "Location" VARCHAR(100),
-	"Indicator" VARCHAR(50),
+	"Indicator" VARCHAR(100),
         "Period" INTEGER,
         "Dim1" VARCHAR(100),
         "First Tooltip" DOUBLE PRECISION
     )
-
+ 
     CREATE TABLE MedicalDoctors(
         "Location" VARCHAR(100),
         "Period" INTEGER,
-        "Indicator" VARCHAR(50),
+        "Indicator" VARCHAR(100),
         "First Tooltip" DOUBLE PRECISION
     )
-
-
+ 
     CREATE TABLE BasicDrinkingWater(
         "Location" VARCHAR(100),
         "Period" INTEGER,
-        "Indicator" VARCHAR(50),
+        "Indicator" VARCHAR(100),
         "First Tooltip" DOUBLE PRECISION
     )
-
-
-
+ 
